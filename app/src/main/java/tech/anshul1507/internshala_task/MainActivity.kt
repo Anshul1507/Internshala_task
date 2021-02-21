e392249884fbf766df6e7c4c859cffb1cb991c82
Anshul1507 <ag1507anshul@gmail.com>
2021-02-21 11:35:34 +0530

    Added: Login Feature

--- app/src/main/java/tech/anshul1507/internshala_task/MainActivity.kt
+++ app/src/main/java/tech/anshul1507/internshala_task/MainActivity.kt
@@ -1,11 +1,39 @@
 package tech.anshul1507.internshala_task
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import tech.anshul1507.internshala_task.databinding.ActivityMainBinding
+import tech.anshul1507.internshala_task.login.LoginFragment
 
-class MainActivity : AppCompatActivity() {
+
+open class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        lateinit var mGoogleSignInClient: GoogleSignInClient
+        var signInCode = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        //login fragment launch
+        val fragLogin = LoginFragment()
+        this.supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragLogin, "LoginFragment").commit()
+
     }
+
 }
---
+++ app/src/main/java/tech/anshul1507/internshala_task/login/LoginFragment.kt
@@ -0,0 +1,65 @@
+package tech.anshul1507.internshala_task.login
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import tech.anshul1507.internshala_task.MainActivity
+import tech.anshul1507.internshala_task.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.signInButton.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent = MainActivity.mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, MainActivity.signInCode)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == MainActivity.signInCode) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            updateUI(account)
+        } catch (e: ApiException) {
+            Log.w("TAG", "signInResult:failed code=" + e.statusCode)
+        }
+    }
+
+    private fun updateUI(acc: GoogleSignInAccount?) {
+        //todo fragment transition to main UI
+        Log.e("test", "login success")
+    }
+
+}
