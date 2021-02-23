cfd5a0a35ba6a2f31b640e446b2928d57cf5a72c
Anshul1507 <ag1507anshul@gmail.com>
2021-02-23 09:18:51 +0530

    Added: Animation while loading recyclerview

--- app/src/main/java/tech/anshul1507/internshala_task/adapter/NoteAdapter.kt
+++ app/src/main/java/tech/anshul1507/internshala_task/adapter/NoteAdapter.kt
@@ -1,15 +1,42 @@
 package tech.anshul1507.internshala_task.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
 import androidx.recyclerview.widget.RecyclerView
 import tech.anshul1507.internshala_task.databinding.ItemNotesBinding
 import tech.anshul1507.internshala_task.entity.NoteModel
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 class NoteAdapter(private val listener: NotesItemClickListener) :
     RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
 
     private val allNotes = ArrayList<NoteModel>()
+
+    private var lastPosition = -1
+    private fun setAnimation(viewToAnimate: View, position: Int) {
+        // If the bound view wasn't previously displayed on screen, it's animated
+        if (position > lastPosition) {
+            val anim = ScaleAnimation(
+                0.0f,
+                1.0f,
+                0.0f,
+                1.0f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f
+            )
+            anim.duration =
+                Random().nextInt(501).toLong() //to make duration random number between [0,501)
+            viewToAnimate.startAnimation(anim)
+            lastPosition = position
+        }
+    }
 
     inner class NoteViewHolder(private val binding: ItemNotesBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -39,6 +66,8 @@ class NoteAdapter(private val listener: NotesItemClickListener) :
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         holder.bind(position)
+        // call Animation function
+        setAnimation(holder.itemView, position)
     }
 
     override fun getItemCount(): Int {
